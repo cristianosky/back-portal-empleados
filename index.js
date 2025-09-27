29fdf12aae0d6b737f427383687a2dcc090c7207
Cristian Villalba <cristianvillalba1999@gmail.com>
2025-09-26 21:25:10 -0500

    Ajuste cors

--- index.js
+++ index.js
@@ -10,11 +10,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors({
-  origin: 'http://localhost:4200', // Cambia esto según tu configuración de frontend
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
+app.use(cors());
 
 app.use(express.json());
 
